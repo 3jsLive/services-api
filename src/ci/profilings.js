19a1fbff60942992bf4e94872a920c9b036cdd17
3cidev <3cidev>
2020-02-19 17:23:32 +0100

    profiling: intial support for profilingConsole

--- src/ci/profilings.js
+++ src/ci/profilings.js
@@ -1,11 +1,13 @@
 const { createHandler } = require( './utils' );
 
 
-const category = 'profiling';
+const category = 'profiles';
 
 module.exports = {
 
 	// stats tracking TODO: WIP debug
-	'/profilingStats/showFile/:sha': createHandler( category, 'statsResults-examples_webgl_loader_gltf' )
+	//	'/profilingStats/showFile/:sha': createHandler( category, 'statsResults-examples_webgl_loader_gltf' ),
+
+	'/profilingConsole/showFile/:sha': createHandler( category, 'ProfConsole' )
 
 };
